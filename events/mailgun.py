d01bb4e550ec8b9d5bb7b7f2e3df360ccba4fd3a
Shaun <shaun.kahler@gmail.com>
2020-06-18 05:55:13 +0930

    removed 'self' from test_send_email

--- events/mailgun.py
+++ events/mailgun.py
@@ -9,7 +9,7 @@ import logging
 import os
 import pickle
 
-def test_send_email(self, sender='postmaster@sandboxd8e83a2acbd844bbbbca06fde883de7b.mailgun.org', recipient=['shaun.kahler@gmail.com'], subject='test', content='success test'):
+def test_send_email(sender='postmaster@sandboxd8e83a2acbd844bbbbca06fde883de7b.mailgun.org', recipient=['shaun.kahler@gmail.com'], subject='test', content='success test'):
         #message = MIMEText(content)
         #payload = {'raw': base64.urlsafe_b64encode(message.as_string().encode('utf-8')).decode('ascii')}
         with mail.get_connection() as connection:
