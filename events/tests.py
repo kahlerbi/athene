cfbbd7ae5629f175fa958bd4f5d9b03931c8d875
Shaun <shaun.kahler@gmail.com>
2020-06-18 04:22:29 +0930

    changed sender to postmaster@sandbox

--- events/tests.py
+++ events/tests.py
@@ -12,7 +12,7 @@ import pickle
 # Create your tests here.
 
 class EmailTestCase(TestCase):
-    def test_send_email(self, sender='shaun.kahler@gmail.com', recipient=['shaun.kahler@gmail.com'], subject='test', content='success test'):
+    def test_send_email(self, sender='postmaster@sandboxd8e83a2acbd844bbbbca06fde883de7b.mailgun.org', recipient=['shaun.kahler@gmail.com'], subject='test', content='success test'):
         #message = MIMEText(content)
         #payload = {'raw': base64.urlsafe_b64encode(message.as_string().encode('utf-8')).decode('ascii')}
         with mail.get_connection() as connection:
