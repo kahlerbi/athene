fb7c3760346e90d9d2a5193eaf3606dc9f7bf904
Shaun <shaun.kahler@gmail.com>
2020-06-23 16:37:01 +0930

    added logger.info() to tasks

--- seekers/tasks.py
+++ seekers/tasks.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
+
 from celery import shared_task
 
 @shared_task
 def print_task():
+    logger.info('Task Succeeded')
     return 'Task Succeeded'
